fb65d6a2dccb1c40989af88aa71aef51b8e19100
Nam Thanh Nguyen <35841468+jkelol111@users.noreply.github.com>
2021-02-08 10:19:35 +0800

    :heavy_plus_sign: Add foobar2000 cask.

---
+++ Casks/foobar2000-jkelol111.rb
@@ -0,0 +1,13 @@
+cask "foobar2000-jkelol111" do
+  version "v2.2.15"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+
+  url "https://www.foobar2000.org/files/foobar2000-#{version}.dmg"
+  name "foobar2000"
+  desc "foobar2000 is an advanced freeware audio player."
+  homepage "https://www.foobar2000.org/"
+
+  depends_on macos: ">= :el-capitan"
+
+  app "foobar2000.app"
+end
