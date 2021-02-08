4c2e53c1ec8d49e26721176e689dbf3cd6ea3df4
Nam Thanh Nguyen <35841468+jkelol111@users.noreply.github.com>
2021-02-08 11:06:56 +0800

    :sparkles: Remove sha256 sum check for deemix-pyweb cask.

--- Casks/deemix-pyweb-jkelol111.rb
+++ Casks/deemix-pyweb-jkelol111.rb
@@ -1,6 +1,6 @@
 cask "deemix-pyweb-jkelol111" do
   version :latest
-  sha256 "a6865011636daab427b05988997764ea6ce8cf4c94c2091dfc800356ecd82695"
+  sha256 :nocheck
 
   url "https://download.deemix.app/pyweb/macosx-10.14-x86_64-latest.zip"
   name "deemix-pyweb"
